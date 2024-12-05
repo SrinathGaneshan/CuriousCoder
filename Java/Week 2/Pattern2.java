cbfe6c132f361b278284d05d689a0143c31cde86
Srinath G <88044278+SrinathGaneshan@users.noreply.github.com>
2024-12-05 11:07:17 +0530

    Update Pattern2.java
    Output
    1 2 3 4
    1 2 3 4
    1 2 3 4
    1 2 3 4

--- Java/Week 2/Pattern2.java
+++ Java/Week 2/Pattern2.java
@@ -1,6 +1,6 @@
 class Pattern2 {
     public static void main(String[] args) {
-        int n=15;
+        int n=4;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 System.out.print(j);
